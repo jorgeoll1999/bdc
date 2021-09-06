98eeab70ccd8dc63561d35da154797e2996efc74
jorgeoll1999 <61676377+jorgeoll1999@users.noreply.github.com>
2021-09-05 21:38:18 -0500

    Update aperturaCrud.php

--- aperturaCrud.php
+++ aperturaCrud.php
@@ -2,7 +2,12 @@
 
 
 
- include './conex.php';
+$conexion = mysqli_connect("sql10.freemysqlhosting.net", "sql10432939", "IBSnzrUE5W","sql10432939");
+
+if (!$conexion) {
+exit("error al intentar conectase al servidor MySQL");
+}
+
 
 
 //insert
